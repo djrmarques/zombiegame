0d818686de101ef7d74f98f6ec5b59141d10a603
david <davidmarques856@gmail.com>
2020-01-03 20:16:02 +0000

    Started working in the Field operations

--- src/main/scala/ZombieGame/Ash.scala
+++ src/main/scala/ZombieGame/Ash.scala
@@ -13,13 +13,12 @@ object Ash {
   def x: Int = _x
   def x_= (newValue: Int) = _x = newValue
   def obX: Int = _obX
-  def obX_= (newValue: Int) = _obX = newValue
+  def obX_ = (newValue: Int) = _obX = newValue
 
   def y: Int = _y
   def y_= (newValue: Int) = _y = newValue
   def obY: Int = _obY
   def obY_= (newValue: Int) = _obY = newValue
-
 
   def objective: String = s"$obX $obY"
 }
--- src/main/scala/ZombieGame/Field.scala
+++ src/main/scala/ZombieGame/Field.scala
@@ -1,7 +1,18 @@
 package ZombieGame
 
+import scala.math.floor
+
 object Field {
   def width: Int =  16000
   def height: Int = 9000
+
+  def convertCoords(x: Int, y: Int): Int = y*width + x
+
+  def reverseConvertCoords(pos: Int) : (Int, Int) = {
+    val y: Int = floor(pos/width).toInt
+    val x: Int = pos - (y*width)
+    (x, y)
+  }
+
 
 }
