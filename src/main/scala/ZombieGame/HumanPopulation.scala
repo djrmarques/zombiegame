c1e7206b8a290b74a544c095cc717200289928ee
David Marques <david@david-t420>
2020-02-08 21:05:01 +0000

    Created the Human Population

--- src/main/scala/ZombieGame/HumanPopulation.scala
+++ src/main/scala/ZombieGame/HumanPopulation.scala
@@ -12,6 +12,14 @@ class Human(val id: Int, val x: Int, val y: Int) {
 }
 
 // Generate the Human population
-object HumanPopulation {
-  ???
+object HumanPopulation extends generation {
+  val nPopulation = 5
+  def generatePopulation : List[Human]= {
+    val coords = randomGenerateCoordinates(nPopulation)
+    coords map ((p: (Int, Int, Int)) => new Human(p._1, p._2, p._3))
+  }
+  val population = generatePopulation
+  def nDead: Int =  (population filter (_.isDead)).length
+  def nAlive: Int =  (population filter (!_.isDead)).length
+  def anyAlive: Boolean = nAlive > 0
 }
