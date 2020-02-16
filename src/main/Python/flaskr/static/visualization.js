87092fe3f0c7a471820e48b7f249edfc44b163a1
David Marques <david@david-t420>
2020-02-16 19:38:16 +0000

    Added flexbox to css

---
+++ src/main/Python/flaskr/static/visualization.js
@@ -0,0 +1,4 @@
+const filePath = "../../../../output/2020-02-16_04-55-07.json"
+d3.json(filePath, function (data) {
+    console.log(data);
+})
--- src/main/Visualization/visualization.js
+++
@@ -1,2 +0,0 @@
-const gameData = d3.json("output/2020-02-16_04-55-07.json")
-console.log(gameData)
