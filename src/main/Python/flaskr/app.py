9706e40d05743ee4e3de1f6a9a3cc476e384c94e
David Marques <david@david-t420>
2020-02-17 18:20:53 +0000

    Written a function that reads the button name

--- src/main/Python/flaskr/app.py
+++ src/main/Python/flaskr/app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/getGameJson')
-def getGameJson():
-    pass
-
-
 @app.route('/')
 def hello():
-    return render_template("main.html")
+    json_files = [file for file in os.listdir("../../../../output") if file.endswith(".json")]
+    return render_template("main.html", files=json_files)
 
 
 if __name__ == '__main__':
--- src/main/Python/flaskr/static/visualization.js
+++ src/main/Python/flaskr/static/visualization.js
@@ -1,4 +1,5 @@
-const filePath = "../../../../output/2020-02-16_04-55-07.json"
-d3.json(filePath, function (data) {
-    console.log(data);
-})
+function getFile(fileJson) {
+    console.log(fileJson)
+}
+
+
