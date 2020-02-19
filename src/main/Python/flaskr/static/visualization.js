ad06a2def1fb16c2bbcfd7ff674a66146f407c0d
David Marques <david@david-t420>
2020-02-19 21:05:02 +0000

    Cleaned pup the javascript a bit

--- src/main/Python/flaskr/static/visualization.js
+++ src/main/Python/flaskr/static/visualization.js
@@ -1,7 +1,9 @@
+// Gets the filename from the server and calls plotGame
 function getFile(fileName) {
-    $.post("/get_game_file", {fileName: fileName}).done(function(json){console.log(json)})
+    $.post("/get_game_file", {fileName: fileName}).done(function(json){plotGame(json)})
 };
 
+// Plots the game using d3.js
 function plotGame(gameJson){
     console.log(gameJson)
 }
