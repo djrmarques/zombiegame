0c978edc67ffaa27f2e35b2191fba62c21a0169d
David Marques <david@david-t420>
2020-02-23 12:30:57 +0000

    Cleaned javascript code

--- src/main/Python/flaskr/static/visualization.js
+++ src/main/Python/flaskr/static/visualization.js
@@ -20,17 +20,17 @@ const x = d3.scaleLinear().domain([0, 16000]).range([radius * 2, width]);
 const y = d3.scaleLinear().domain([0, 9000]).range([height, radius * 2]);
 
 // Variable with the turn
-var turn = 0
+var turn = 0;
 
 // function that outputs 0 if the target is dead, else the radious
-function deadRadius(isDead){if (isDead==1) return 0; else return radius}
+function deadRadius(isDead){if (isDead===1) return 0; else return radius}
 
 // Run the create SVG function when the page fully loads
 function createSVG() {
-    svg = d3.select("#game-viz").append("svg")
+    let svg = d3.select("#game-viz").append("svg")
         .attr("width", width)
         .attr("id", "svgViz")
-        .attr("height", height)
+        .attr("height", height);
 
     svg.append("rect")
         .attr("x", 0)
@@ -38,8 +38,7 @@ function createSVG() {
         .attr("width", width)
         .attr("height", height)
         .attr("fill", "none")
-        .attr('stroke', 'black')
-    ;
+        .attr('stroke', 'black') ;
 }
 
 // Gets the filename from the server and calls plotGame
@@ -50,7 +49,7 @@ function getFile(fileName) {
 }
 
 // Creates the d3 svg objects and the svg element
-function createPos(turnData, selectName, fill){
+function createPos(turnData, selectName, fill, svg, attackRange){
 
     svg.selectAll(selectName)
         .data(turnData[selectName])
@@ -68,6 +67,7 @@ function createPos(turnData, selectName, fill){
         .attr("fill", fill)
         .attr("class", selectName)
         .attr("r", radius)
+
 }
 
 
@@ -83,9 +83,9 @@ function plotGame(gameJson) {
     let turnData = gameJson[0];
     /* Zombie location */
 
-    createPos(turnData, "zombies", zombiePos)
-    createPos(turnData, "humans", humanPos)
-    createPos(turnData, "Ash", ashPos)
+    createPos(turnData, "zombies", zombiePos, svg, zombieRange);
+    createPos(turnData, "humans", humanPos, svg, 0);
+    createPos(turnData, "Ash", ashPos, svg, ashRange);
 
     turn = 1;
     while (turn < gameJson.length) {
@@ -96,8 +96,7 @@ function plotGame(gameJson) {
     }
 }
 
-
-function updatePos(turn, turnData, char, fill, attackRange){
+function updatePos(turn, turnData, char, fill){
     char.transition().delay(duration*turn).duration(duration)
         .attr("cx", function (d) {
             return x(d["posX"]);
@@ -116,16 +115,16 @@ function updatePos(turn, turnData, char, fill, attackRange){
 function plotTurn(turn, turnData, svg) {
 
     /* Zombie location */
-    let zombies = svg.selectAll(".zombies").data(turnData["zombies"])
-    updatePos(turn, turnData, zombies, zombiePos, zombieRange)
+    let zombies = svg.selectAll(".zombies").data(turnData["zombies"]);
+    updatePos(turn, turnData, zombies, zombiePos);
 
     /* Human location */
-    let humans = svg.selectAll(".human").data(turnData["humans"])
-    updatePos(turn, turnData, humans, humanPos, 0)
+    let humans = svg.selectAll(".human").data(turnData["humans"]);
+    updatePos(turn, turnData, humans, humanPos);
 
     /* Ash location */
-    let ash = svg.selectAll(".Ash").data(turnData["Ash"])
-    updatePos(turn, turnData, ash, ashPos, ashRange)
+    let ash = svg.selectAll(".Ash").data(turnData["Ash"]);
+    updatePos(turn, turnData, ash, ashPos);
 }
 
 
