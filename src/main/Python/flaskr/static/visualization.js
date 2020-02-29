467407554e2103ebc6330f3ab5d2e565013c8074
David Marques <david@david-t420>
2020-02-29 10:34:12 +0000

    Added id identifiers

--- src/main/Python/flaskr/static/visualization.js
+++ src/main/Python/flaskr/static/visualization.js
@@ -23,6 +23,9 @@ const duration = 500;
 const x = d3.scaleLinear().domain([0, 16000]).range([radius * 2, width]);
 const y = d3.scaleLinear().domain([0, 9000]).range([height, radius * 2]);
 
+// fontsize
+const fontSize = 10
+
 // Variable with the turn
 var turn = 0;
 
@@ -34,6 +37,10 @@ function deadRadius(isDead) {
 // function that outputs 0 if the the character is dead
 function deadStroke(isDead) {
     if (isDead === 1) return 0; else return lineStroke
+}
+
+function deadText(isDead){
+    if (isDead === 1) return 0; else return fontSize;
 }
 
 
@@ -106,6 +113,24 @@ function createPos(turnData, selectName, fill, attackRange) {
             .attr("stroke-width", lineStroke)
             .attr("class", selectName + "Line")
     }
+
+    if (selectName == "zombies" || selectName == "humans") {
+        svg.selectAll(selectName + "Text")
+            .data(turnData[selectName])
+            .enter()
+            .append("text")
+            .attr("x", function (d) {
+                return x(d["posX"])-radius/2;
+            })
+            .attr("y", function (d) {
+                return y(d["posY"])+radius/2;
+            })
+            .text(function (d) {
+                return d["id"];
+            })
+            .attr("class", selectName + "Text")
+            .attr("font-size", fontSize)
+    }
 }
 
 
@@ -173,34 +198,69 @@ function updatePos(turn, turnData, selectName, fill) {
                 return deadStroke(d["isDead"])
             })
     }
+
+    if (selectName == "zombies" || selectName == "humans") {
+        svg.selectAll("."+selectName + "Text")
+            .data(turnData[selectName])
+            .transition().duration(duration)
+            .attr("x", function (d) {
+                return x(d["posX"])-radius/2;
+            })
+            .attr("y", function (d) {
+                return y(d["posY"])+radius/2;
+            })
+            .attr("font-size", function(d) {
+            return deadText(d["isDead"]);
+            })
+    }
 }
 
 // Plots the position specific for a given turn
-    function plotTurn(turn) {
+function plotTurn(turn) {
 
-        let turnData = globalGameJson[turn]
-        printStatus(turnData)
+    let turnData = globalGameJson[turn]
+    printStatus(turnData)
 
-        // Display which turn it is
-        $("#turn").text(turn)
+    // Display which turn it is
+    $("#turn").text(turn)
 
 
-        /* Zombie location */
-        updatePos(turn, turnData, "zombies", zombiePos);
+    /* Zombie location */
+    updatePos(turn, turnData, "zombies", zombiePos);
 
-        /* Ash location */
-        updatePos(turn, turnData, "Ash", ashPos);
+    /* Ash location */
+    updatePos(turn, turnData, "Ash", ashPos);
 
-        /* Human location */
-        updatePos(turn, turnData, "humans", humanPos);
+    /* Human location */
+    updatePos(turn, turnData, "humans", humanPos);
 
-    }
+}
 
 // Print the status of every element on the screen
 function printStatus(turnData){
-    console.log("Turn: ", turn)
-    console.log(turnData)
-    console.log(turnData)
+    // console.log("Turn: ", turn);
+    // console.log(turnData);
+    // console.log(turnData);
+
+    // Reset the content of the status
+    $("#status-log").content = "";
+
+    const reducer = (acc, item) => {return acc + item};
+
+    // Empty the previous logs
+    $("#status-log").empty();
+
+    // Print Ash status
+    let ashStats = turnData["Ash"][0];
+    let ashString = Object.keys(ashStats).map(d => {return d + ": " + ashStats[d] + " | "}).reduce(reducer, "");
+    $("#status-log").append("ASH: " + ashString);
+
+    let i;
+    for (i=0; i<turnData["zombies"].length; i++){
+        let zombieStats = turnData["zombies"][i];
+        let zombieString = Object.keys(zombieStats).map(d => {return d + ": " + zombieStats[d] + " | "}).reduce(reducer, "");
+        $("#status-log").append("<p> Zombie" + i + ": " + zombieString + "<p>");
+    }
 }
 
 
